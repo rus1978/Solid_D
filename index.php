3831071193f68bd89361b6c48972c799205c9c82
rus1978 <BondarRuslanVladimirovich@gmail.com>
2020-02-15 23:03:44 +0200

    create

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * SOLID — буква «D»
+ *
+ * Принцип инверсии зависимостей (The Dependency Inversion Principle)
+ * «Зависимость на Абстракциях. Нет зависимости на что-то конкретное.»
+ *
+ * Модули верхних уровней не должны зависеть от модулей нижних уровней. Оба типа модулей должны зависеть от абстракций.
+ * Абстракции не должны зависеть от деталей. Детали должны зависеть от абстракций.
+ */
+
+
+interface ISave
+{
+    public function insert($date);
+}
+
+class SaveDb implements ISave
+{
+    public function insert($date)
+    {
+        echo $date.' сохранено в БД';
+    }
+}
+
+class SaveFile implements ISave{
+    public function insert($date)
+    {
+        echo $date.' сохранено в файл';
+    }
+}
+
+class BaseClass
+{
+    public function save(ISave $obj, $date)
+    {
+        $obj->insert($date);
+    }
+}
+
+class SaveSession /*implements Save */{//будет ошибка т.к. закомментирован интерфейс
+    public function insert($date)
+    {
+        echo $date.' сохранено в сессию';
+    }
+}
